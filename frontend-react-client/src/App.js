245437337267ec63a4e22b969ae9f6cf8e5f127f
philippwulff <philipp.sylt@gmail.com>
2023-11-13 18:50:59 +0100

    cleanup

--- frontend-react-client/src/App.js
+++ frontend-react-client/src/App.js
@@ -7,28 +7,6 @@ import Navbar from './components/navbar';
 
 function App() {
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //       <p>The current time is {currentTime}.</p>
-  //     </header>
-  //   </div>
-  // );
-
-
-
   return (
     <div className="App">
       <header className="App-header">
